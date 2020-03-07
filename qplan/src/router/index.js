f22e0df7e0089509df32428c05248555cd27b3b9
Jack Williams <jackw667@gmail.com>
2020-03-07 17:46:01 -0500

    Add search view

--- qplan/src/router/index.js
+++ qplan/src/router/index.js
@@ -10,6 +10,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import('../views/Search.vue')
+  },
 ]
 
 const router = new VueRouter({
