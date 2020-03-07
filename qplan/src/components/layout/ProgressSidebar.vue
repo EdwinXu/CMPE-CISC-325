2a1a430c5ecf9bf235e2222fc58f2688780fb96b
Jack Williams <jackw667@gmail.com>
2020-03-07 18:51:22 -0500

    Add button to detailed progress page

--- qplan/src/components/layout/ProgressSidebar.vue
+++ qplan/src/components/layout/ProgressSidebar.vue
@@ -4,6 +4,15 @@
       Credit Progress
       <v-icon color="black" class="mb-1">mdi-progress-check</v-icon>
     </div>
+    PUT PROGRESS BARS HERE
+    <div class="mx-1">
+      <v-btn rounded block color="indigo lighten-1">
+        <router-link to="/progress">
+          Detailed Progress
+          <v-icon>mdi-progress-check</v-icon>
+        </router-link>
+      </v-btn>
+    </div>
   </v-navigation-drawer>
 </template>
 
