13c5d151c2df1ba0485dce47b1750438511c6b9d
Jack Williams <jackw667@gmail.com>
2020-03-07 18:17:11 -0500

    Add vue template to progress and search pages

---
+++ qplan/src/views/Progress.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Put detailed progress info here
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Progress",
+}
+</script>
+
+<style>
+
+</style>
---
+++ qplan/src/views/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Put search page content here
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+}
+</script>
+
+<style>
+
+</style>
