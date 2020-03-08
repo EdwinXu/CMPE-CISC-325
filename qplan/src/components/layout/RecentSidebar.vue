c18d309f5d11cb52f1455fc7b52e08ec731ae76e
Jack Williams <jackw667@gmail.com>
2020-03-08 17:30:38 -0400

    Add transitions to the recent courses list

--- qplan/src/components/layout/RecentSidebar.vue
+++ qplan/src/components/layout/RecentSidebar.vue
@@ -2,10 +2,12 @@
   <v-navigation-drawer app right width="300" clipped v-model="recentSidebar">
     <span class="headline ml-1">Recent Courses</span>
 
-    <div v-for="course in recentlyViewedCourses" :key="course.id" class="ma-1">
-      <CourseCard :course="course" />
-    </div>
-
+    <v-fade-transition group>
+      <div v-for="course in recentlyViewedCourses" :key="course.id" class="ma-1">
+        <CourseCard :course="course" />
+      </div>
+    </v-fade-transition>
+    
   </v-navigation-drawer>
 </template>
 
