fbcab4aff5bc81a53d44795d0e7c3936cb27793e
Jack Williams <jackw667@gmail.com>
2020-03-07 20:28:42 -0500

    Add section breaks for progress sidebar

--- qplan/src/components/layout/ProgressSidebar.vue
+++ qplan/src/components/layout/ProgressSidebar.vue
@@ -4,8 +4,29 @@
       Credit Progress
       <v-icon color="black" class="mb-1">mdi-progress-check</v-icon>
     </div>
+
+    <div class="subtitle-1">
+      <b>Total Credits</b>
+    </div>
     <ProgressBar />
-    <div class="mx-1">
+
+    <v-divider class="mb-2 mt-3"></v-divider>
+
+    <v-row class="mx-0">
+      <b>Accreditation Units (AUs)</b>
+    </v-row>
+
+    <ProgressBar />
+
+    <v-divider class="mb-2 mt-3"></v-divider>
+
+    <v-row class="mx-0">
+      <b>Additional Requirements</b>
+    </v-row>
+
+    <ProgressBar />
+
+    <div class="pt-3 mb-4 mx-1">
       <v-btn rounded block color="indigo lighten-1">
         <router-link to="/progress">
           Detailed Progress
